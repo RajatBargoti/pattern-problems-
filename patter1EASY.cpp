88c7421e4e999bcc5729fad41af4ac99ef84ebfc
RAJAT BARGOTI <87090584+RajatBargoti@users.noreply.github.com>
2022-12-05 12:43:19 +0530

    Add files via upload

---
+++ patter1EASY.cpp
@@ -0,0 +1,25 @@
+/*
+1234
+1234
+1234
+1234 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int i=1;
+    cin>>n;
+    while(i<=n){
+         int j=1;
+         while(j<=n){
+            cout<<j;
+            j++;
+         }
+         cout<<endl;
+         i++;
+    }
+}
